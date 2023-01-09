6888122dd5a260d9e8d579b50847597de5c9b7cf
suryansh1801 <87513522+suryansh1801@users.noreply.github.com>
2023-01-09 23:18:24 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Education from "./components/Education";
+
+export default function App() {
+  return (
+    <main className="text-gray-300 bg-purple-600 body-font">
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Education />
+      <Contact />
+    </main>
+  );
+}
